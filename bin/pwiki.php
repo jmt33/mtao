c1af96a98f02584fdbe0a7053300991b95efc640
mtao <jmtao33@gmail.com>
2016-05-27 23:51:49 +0800

    pwiki as a composer

---
+++ bin/pwiki.php
@@ -0,0 +1,16 @@
+<?php
+$dir = __DIR__;
+include_once $dir."/../vendor/autoload.php";
+
+$bootstrap = new \Pwiki\Bootstrap();
+$commentPlugin = "<div id=\"uyan_frame\"></div><script type=\"text/javascript\" src=\"http://v2.uyan.cc/code/uyan.js?uid=2101665\"></script>";
+$bootstrap->setConfig(
+    array(
+        'markdownPath' => $dir.'/../markdown/',
+        'htmlPath' => $dir.'/../html/',
+        'htmlIndexFile' => $dir.'/../index.html',
+        'databaseFile' => $dir.'/../.log.json',
+        'commentPlugin' => $commentPlugin
+    )
+);
+$bootstrap->run();
