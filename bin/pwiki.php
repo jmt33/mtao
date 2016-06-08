d6e7633d47153f8cc539f83c42f31f1381f72651
mtao <jmtao33@gmail.com>
2016-06-08 20:38:33 +0800

    update bin

--- bin/pwiki.php
+++ bin/pwiki.php
@@ -5,12 +5,17 @@ include_once $dir."/../vendor/autoload.php";
 $bootstrap = new \Pwiki\Bootstrap();
 $commentPlugin = "<div id=\"uyan_frame\"></div><script type=\"text/javascript\" src=\"http://v2.uyan.cc/code/uyan.js?uid=2101665\"></script>";
 $bootstrap->setConfig(
-    array(
+    [
+        'pageInfo' => [
+            'title' => 'Mtao Blog',
+            'keywords' => '',
+            'description' => ''
+        ],
         'markdownPath' => $dir.'/../markdown/',
         'htmlPath' => $dir.'/../html/',
         'htmlIndexFile' => $dir.'/../index.html',
         'databaseFile' => $dir.'/../.log.json',
         'commentPlugin' => $commentPlugin
-    )
+    ]
 );
 $bootstrap->run();
